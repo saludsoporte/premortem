5ab3a6991f9c298a1208cf1a4a5f8cd1f7f526b9
salud.aplicaciones@gmail.com <salud.aplicaciones@gmail.com>
2025-07-03 09:49:08 -0600

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,21 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "popper"
+import "bootstrap"
+
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
+
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if(toastTrigger){
+    toastTrigger.addEventListener('click',function(){
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+}
---
+++ config/importmap.rb
@@ -0,0 +1,12 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "signin", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "popper",to: 'popper.js',preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+#pin "bootstrap" # @5.3.7
+#pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
