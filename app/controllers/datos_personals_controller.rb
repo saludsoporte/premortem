375b52df164a349c098c833ebd582ed8e1c8575c
salud.aplicaciones@gmail.com <salud.aplicaciones@gmail.com>
2025-08-11 15:48:29 -0600

    .

--- app/controllers/datos_personals_controller.rb
+++ app/controllers/datos_personals_controller.rb
@@ -1,5 +1,37 @@
 class DatosPersonalsController < ApplicationController
   def guardar_datos
-    adad
+    if validar
+      # Guardar los datos
+      datos = DatosPersonal.new(
+        nombre: params[:nombre],
+        apellidos: params[:apellidos],
+        puesto: params[:puesto],
+        titulo: params[:titulo],
+        user_id:params[:user]
+      )
+      if datos.save
+        redirect_to root_path(datos:true)
+      else
+        redirect_to root_path(datos:false)
+      end
+    else
+      ssasdasd
+      redirect_to root_path(datos:false,error:true)
+    end    
+  end
+  def validar    
+    bandera = true
+    if params[:nombre].blank?      
+      bandera = false
+    elsif params[:apellidos].blank?      
+      bandera = false
+    elsif params[:puesto].blank?      
+      bandera = false
+    elsif params[:titulo].blank?      
+      bandera = false
+    else
+      bandera = true
+    end
+    return bandera
   end
 end
