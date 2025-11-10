5ac5d31cda24b8f89e270c619b02cb911aecba33
salud.aplicaciones@gmail.com <salud.aplicaciones@gmail.com>
2025-11-10 09:26:50 -0600

    hotfix

---
+++ db/migrate/20251110152430_change_titulo.rb
@@ -0,0 +1,6 @@
+class ChangeTitulo < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :datos_personals, :titulo, :unidad
+    #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_04_171102) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_10_152430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,7 +104,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_04_171102) do
     t.string "nombre"
     t.string "apellido_paterno"
     t.string "puesto"
-    t.string "titulo"
+    t.string "unidad"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
